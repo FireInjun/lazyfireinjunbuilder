abd612bdafe908c477990b900c2c9a6a1939bad2
Mykeal Kenny <mk@fireinjun.com>
2018-06-20 12:58:35 -0400

    modified bat

--- builditbaby.bat
+++ builditbaby.bat
@@ -2,7 +2,10 @@
 @ECHO OFF
 :BEGIN
 CLS
-CHOICE /N /C:123 /M "what do you want to be lazy with? (1, 2, or 3)"%1
+ECHO 1: React
+ECHO 2: React plus Node
+ECHO 3: React plus Node plus Mongo
+CHOICE /N /C:123 /M "what do you want to be lazy with? ( 1, 2, 3 )"%1
 IF ERRORLEVEL ==3 GOTO THREE
 IF ERRORLEVEL ==2 GOTO TWO
 IF ERRORLEVEL ==1 GOTO ONE
@@ -17,3 +20,4 @@ GOTO END
 ECHO Well, you made an app maybe.... 
 :END
 pause
+REM asfas
